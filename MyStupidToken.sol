db9f5c04acba6504609515229d4fbcb5b966677b
Julien Béranger <julien@strat.cc>
2021-01-11 16:38:27 +0100

    Update MyStupidToken.sol
    Upgrade to Solidity 0.8.0

--- MyStupidToken.sol
+++ MyStupidToken.sol
@@ -1,11 +1,12 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.6.0 <0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
 contract MyStupidToken is ERC20
 {
-    constructor()
-	public ERC20("My Stupid Token", "MST")
+    constructor() ERC20("My Stupid Token", "MST")
 	{
 		_mint(msg.sender, 10000*10**18);
 	}
